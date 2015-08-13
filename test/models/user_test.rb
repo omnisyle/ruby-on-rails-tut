fb84527646cd4409fc1a518b3a12e441f4d8f859
Si Le <omnisyle@gmail.com>
2015-08-13 17:09:20 +0700

    added test for downcase email and change regex expression

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -58,6 +58,14 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
+  test 'email should be saved lowercase' do
+    mixed_case_email = 'test@aAaaXam.com'
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+
   test 'password should be present' do
     @user.password = @user.password_confirmation = ' ' * 6
     assert_not @user.valid?

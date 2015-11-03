64690adea4089d4e784f8f8f885372e9e83178ee
Omnisyle <omnisyle@gmail.com>
2015-11-03 23:48:00 +0700

    Adds disclaimer

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,6 +9,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
     storage :fog
+    #storage :file
   else
     storage :file
   end
